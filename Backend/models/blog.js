65dce7254666df93e0b7f80923138eba7ed5e624
abhinav2210103 <abhinav2210103@akgec.ac.in>
2024-07-24 11:36:07 +0530

    Backend : Added Like Functionality to blogs

--- Backend/models/blog.js
+++ Backend/models/blog.js
@@ -15,6 +15,10 @@ const blogSchema = new mongoose.Schema({
     type: String,
     required: true,
   },
+  likesCount: {
+    type: Number,
+    default: 0,
+  },
   suggestedTopic: {
     type: String,
   },
