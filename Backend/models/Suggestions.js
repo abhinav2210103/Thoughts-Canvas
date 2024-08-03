b18dd926eba328487aa06f8190494d78cc85bfc3
abhinav2210103 <abhinavmishra0493>
2024-08-03 20:03:45 +0530

    Backend: Added Topic Suggestion Model

---
+++ Backend/models/Suggestions.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose');
+
+const topicSuggestionSchema = new mongoose.Schema({
+    suggestedBy: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'user',
+        required: true,
+    },
+    suggestionText: {
+        type: String,
+        required: true,
+    },
+    imageUrl: {
+        type: String,
+    },
+    status: {
+        type: String,
+        enum: ['PENDING', 'APPROVED', 'REJECTED'],
+        default: 'PENDING',
+    },
+}, { timestamps: true });
+
+const TopicSuggestion = mongoose.model('TopicSuggestion', topicSuggestionSchema);
+
+module.exports = TopicSuggestion;
