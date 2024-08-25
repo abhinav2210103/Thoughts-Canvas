f2271d64d22c2f339306a16203af902fe65f2eb0
abhinav2210103 <abhinavmishra0493>
2024-08-25 18:10:45 +0530

    Backend : Implement clustering for improved performance and scalability

--- Backend/app.js
+++ Backend/app.js
@@ -4,6 +4,8 @@ const cors = require("cors");
 const { connectMongoDB } = require("./connection");
 const cookieParser = require("cookie-parser");
 const compression = require('compression');
+const cluster = require('node:cluster');
+const os = require('os');
 const userRouter = require("./routes/user");
 const blogRouter = require("./routes/blog");
 const topicRouter = require("./routes/topic");
@@ -15,30 +17,44 @@ const {
 
 const app = express();
 const PORT = process.env.PORT || 8000;
+const numCPUs = os.cpus().length;
 
-connectMongoDB(process.env.MONGO_URL).then((e) =>
-  console.log("MongoDB Connected")
-);
+if (cluster.isPrimary) {
+  console.log(`Primary ${process.pid} is running`);
 
-app.use(
-  cors({
-    origin: "http://localhost:5173",
-    methods: ["GET", "POST"],
-    allowedHeaders: ["Content-Type", "Authorization"],
-    credentials: true,
-  })
-);
+  for (let i = 0; i < numCPUs; i++) {
+    cluster.fork();
+  }
 
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.json());
+  cluster.on('exit', (worker, code, signal) => {
+    console.log(`Worker ${worker.process.pid} died`);
+    cluster.fork();
+  });
 
+} else {
+  connectMongoDB(process.env.MONGO_URL).then((e) =>
+    console.log("MongoDB Connected")
+  );
 
-app.use("/user", userRouter);
-app.use(checkForAuthenticationCookie("token"));
-app.use("/blog", blogRouter);
-app.use("/topic", topicRouter);
-app.use("/admin", adminRouter);
-app.use("/comment", commentRouter);
+  app.use(
+    cors({
+      origin: "http://localhost:5173",
+      methods: ["GET", "POST"],
+      allowedHeaders: ["Content-Type", "Authorization"],
+      credentials: true,
+    })
+  );
 
-app.listen(PORT, () => console.log(`Server is started at ${PORT}`));
+  app.use(express.urlencoded({ extended: false }));
+  app.use(cookieParser());
+  app.use(express.json());
+
+  app.use("/user", userRouter);
+  app.use(checkForAuthenticationCookie("token"));
+  app.use("/blog", blogRouter);
+  app.use("/topic", topicRouter);
+  app.use("/admin", adminRouter);
+  app.use("/comment", commentRouter);
+
+  app.listen(PORT, () => console.log(`Worker ${process.pid} started and listening on port ${PORT}`));
+}
