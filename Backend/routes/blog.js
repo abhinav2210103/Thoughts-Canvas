e9c1fe99de9908513396190f365c65f0ea590784
Abhinav Mishra <abhinavmishra0493@gmail.com>
2024-09-23 00:51:54 +0530

    Backend : Refactor like api route to use GET instead of POST

--- Backend/routes/blog.js
+++ Backend/routes/blog.js
@@ -6,6 +6,6 @@ router.post("/addnew",handleAddNewBlog);
 router.get('/all', handleGetAllBlogs);
 router.post('/like/:id',handleLikeCount);
 router.post('/unlike/:id',handleUnLikeCount);
-router.post('/likes',handleGetAllLike);
+router.get('/likes',handleGetAllLike);
 
 module.exports = router;
